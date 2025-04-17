684ac4fdc7099964bda85275ac4c7e1fb2449540
vipinyadav01 <vipinyadav9m@gmail.com>
2025-04-17 23:09:03 +0530

    Update service worker cache revision for improved asset management

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f001acab'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c0bq3r4otrg"
+    "revision": "0.kcktmiu6lpo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
