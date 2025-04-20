72417f53424ac8f99e9aac191eb264dd5726bb5f
vipinyadav01 <vipinyadav9m@gmail.com>
2025-04-21 02:47:58 +0530

    refactor: Remove unnecessary comment for middleware in index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,6 @@ const serverless = require('serverless-http');
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
