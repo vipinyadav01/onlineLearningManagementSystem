6229652fdeb97f5908ca9c9edacfdf1940ed05a8
Vipin Yadav <130680245+vipinyadav01@users.noreply.github.com>
2025-04-21 01:44:10 +0530

    Delete server directory

--- server/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUserOrders } = require('../controllers/orderController');
-const { authMiddleware } = require('../middleware/authMiddleware');
-
-router.get('/my-orders', authMiddleware, getUserOrders);
-
-module.exports = router;
--- server/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../controllers/paymentController');
-const { authMiddleware } = require('../middleware/authMiddleware');
-
-router.post('/create-order', authMiddleware, paymentController.createOrder);
-router.post('/verify-payment', authMiddleware, paymentController.verifyPayment);
-
-module.exports = router;
