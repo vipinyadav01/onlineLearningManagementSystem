0147c192d2b45696d1e9994a0a79834411359114
vipinyadav01 <vipinyadav9m@gmail.com>
2025-04-21 09:17:29 +0530

    refactor: Remove static file serving for uploads in index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // MongoDB connection
 let isConnected = false;
